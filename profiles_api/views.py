2054f2885764c4841b05623eeb4e804bf826e8c0
OmUniyal <56249146+OmUniyal@users.noreply.github.com>
2019-12-18 16:44:28 +0530

    created a demo API view, changed project urls.py file  and added and configured urls.py file in the application

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HelloAPIView(APIView):
+    '''
+    Test API View
+    '''
+
+    def get(self, request, format = None):
+        '''
+        Returns a list of API view features
+        '''
+
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'is similar to traditional django view',
+            'gives control over application\'s logic',
+            'is mapped manually to URLs',
+        ]
+
+        return Response({'message':'Hello!', 'an_apiview': an_apiview})
