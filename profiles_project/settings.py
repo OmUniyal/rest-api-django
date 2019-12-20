735ed3040966adf0ff9588d377cac0b21bf8ba26
OmUniyal <56249146+OmUniyal@users.noreply.github.com>
2019-12-20 12:08:32 +0530

    Update settings.py

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2&zzyy@p4+^$d3c0-1!m*0j@4xl-7gcg5n56&2j+dclb%^q906'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
